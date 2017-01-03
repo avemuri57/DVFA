4c7a101ee6e4d6ec5f895da906a8e903609b5bc7
Anand Vemuri <avemuri57@Anands-MacBook-Pro.local>
2017-01-03 13:34:25 -0500

    added vulns

--- app.py
+++ app.py
@@ -4,26 +4,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('login.html')
 
 @app.route('/about')
 def about():
-	return 'About Page'
+	return render_template('about.html')
 
 @app.route('/register')
 def register():
 	return 'Registration Page'
 
 #Restricted Routes
-@app.route('/profile')
-def profile():
-	return 'Profile Based on Users Role'
+@app.route('/dashboard')
+def dashboard():
+	return render_template('index.html')
 
 @app.route('/settings')
 def settings():
 	return 'profile Settings'
 
 
+#Admin routes
+@app.route('/YWRtaW5pc3RyYXRvcg==')
+def admin_login():
+	return 'admin login panel'
 
 
 

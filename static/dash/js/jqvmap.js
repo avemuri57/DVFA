737de6c537e52dee43ebc3ec78aaa259af1addda
Anand Vemuri <avemuri57@Anands-MacBook-Pro.local>
2017-01-03 14:32:17 -0500

    UI Updates

---
+++ static/dash/js/jqvmap.js
@@ -0,0 +1,14 @@
+ $(document).ready(function(){
+        $('#world-map-gdp').vectorMap({
+            map: 'world_en',
+            backgroundColor: null,
+            color: '#ffffff',
+            hoverOpacity: 0.7,
+            selectedColor: '#666666',
+            enableZoom: true,
+            showTooltip: true,
+            values: sample_data,
+            scaleColors: ['#E6F2F0', '#149B7E'],
+            normalizeFunction: 'polynomial'
+        });
+      });
---
+++ static/dash/js/skycons-new.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+        var icons = new Skycons({
+            "color": "#73879C"
+          }),
+          list = [
+            "clear-day", "clear-night", "partly-cloudy-day",
+            "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+            "fog"
+          ],
+          i;
+
+        for (i = list.length; i--;)
+          icons.set(list[i], list[i]);
+
+        icons.play();
+      });

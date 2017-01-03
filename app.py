f1719fe0685437b58dda5729ed48b2b1ac3430e1
Anand Vemuri <avemuri57@Anands-MacBook-Pro.local>
2017-01-03 12:09:59 -0500

    Updates login page

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request,render_template
+from flask import Flask, request,render_template,url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Login Page'
+	return render_template('index.html')
 
 @app.route('/about')
 def about():

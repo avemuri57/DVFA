e6138d59b43ecd613870538783b2f5497e1ca47c
Anand Vemuri <avemuri57@Anands-MacBook-Pro.local>
2017-01-26 19:55:48 -0500

    Updates

---
+++ dvfa/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+class Config():
+	SECRET_KEY = 'ThisIsASecret'
+	Debug=True
+	DB_USERNAME='dbuser'
+	DB_PASSWORD=''
+	DATABASE_NAME='dvfa'
+	DB_HOST=os.getenv('IP','0.0.0.0')
+	DB_URI="mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME,DB_PASSWORD,DB_HOST,DATABASE_NAME) 
+	SQLALCHEMY_DATABASE_URI = DB_URI	

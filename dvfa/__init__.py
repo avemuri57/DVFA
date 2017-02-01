89eb80e8b5ebe43b535c19c63a90aa8a3f420eb1
Anand Vemuri <avemuri57@Anands-MacBook-Pro.local>
2017-02-01 18:51:18 -0500

    updated

--- dvfa/__init__.py
+++ dvfa/__init__.py
@@ -1,7 +1,10 @@
+import sys
 from flask import Flask
 from flask_login import LoginManager
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+
+sys.dont_write_bytecode = True
 
 app = Flask(__name__)
 app.config.from_object(Config())
